21628f7ab3aaa97d3ac59af6936190ef8d0a85c6
Pandau <pandau.ting@gmail.com>
2024-11-26 17:21:39 +0100

    remove maxAge

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -111,7 +111,7 @@ export const getToken = (req, res) => {
 
 				// send Cookie
 				res.cookie('session-token', token, {
-					maxAge: 24 * 60 * 60 * 1000,
+					// maxAge: 24 * 60 * 60 * 1000,
 					httpOnly: true,
 					sameSite: 'lax',
 					secure: false,
